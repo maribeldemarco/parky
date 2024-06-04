1616e06a6db9faa42a3ba930b2ded3edfa32ae20
Nadia Sofía Higa <115573711+Sofiahiga@users.noreply.github.com>
2024-06-04 02:18:30 -0300

    Update tab5-routing.module.ts

--- parky/src/app/profile/tab5-routing.module.ts
+++ parky/src/app/profile/tab5-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ProfilePage } from './tab5.page';
+import { Tab5Page } from './tab5.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: ProfilePage
+    component: Tab5Page
   }
 ];
 
@@ -14,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class ProfilePageRoutingModule {}
+export class Tab5PageRoutingModule {}
