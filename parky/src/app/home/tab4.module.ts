02de13a2e87c8b44b25695f3c3e01f32cadafbf8
Nadia Sofía Higa <115573711+Sofiahiga@users.noreply.github.com>
2024-06-04 02:16:41 -0300

    Actualizando pantallas

--- parky/src/app/home/tab4.module.ts
+++ parky/src/app/home/tab4.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-
 import { Tab4PageRoutingModule } from './tab4-routing.module';
+import { HomePage } from './tab4.page';
 
-import { Tab4Page } from './tab4.page';
-
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage
+  }
+];
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    Tab4PageRoutingModule
+    Tab4PageRoutingModule,
+    RouterModule.forChild(routes)
   ],
-  declarations: [Tab4Page]
+  declarations: [
+    HomePage
+  ]
+
 })
 export class Tab4PageModule {}
