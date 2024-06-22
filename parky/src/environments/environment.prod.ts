dd689008d5f7d7745f410bf4b290baf4e38e156e
Christian Gabor <chrisgabor95@gmail.com>
2024-06-22 17:48:21 -0300

    detalle enviroment

--- parky/src/environments/environment.prod.ts
+++ parky/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAIeokwp0IGLSh2c5KXZwMEnmca3Q3-V0U",
+    authDomain: "parky-d6de6.firebaseapp.com",
+    projectId: "parky-d6de6",
+    storageBucket: "parky-d6de6.appspot.com",
+    messagingSenderId: "776592776393",
+    appId: "1:776592776393:web:154b2178447f1614634118",
+    measurementId: "G-FQ3M3GL61M",
+  }
 };
